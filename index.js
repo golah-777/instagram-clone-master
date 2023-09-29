167c626954d8c6757d01e4d36c5777ae3c6ff1f9
golah-777 <128902542+golah-777@users.noreply.github.com>
2023-09-29 15:25:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxN1qjNisUOE-3ckuRNsW6Ng9e4oGoK4Q",
+    authDomain: "instagram-93881.firebaseapp.com",
+    projectId: "instagram-93881",
+    storageBucket: "instagram-93881.appspot.com",
+    messagingSenderId: "951481592496",
+    appId: "1:951481592496:web:75b1232e630c6067fce975"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
